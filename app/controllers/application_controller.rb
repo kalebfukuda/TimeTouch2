35dac6073c4776c52e5e39c2dcfd36ee47d783ff
kaleb fukuda <kalebfukudajp@gmail.com>
2025-10-06 11:01:10 +0900

    Adjusted login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    main_path # 👈 change this to wherever you want
+  end
 end
