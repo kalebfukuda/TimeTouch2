cd5ff5755b398c8744080e8631c211e10e5f8c23
kaleb fukuda <kalebfukudajp@gmail.com>
2025-10-15 18:12:21 +0900

    Changed login style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:home]
+  before_action :authenticate_user!, only: [:main]
 
   def home
   end
