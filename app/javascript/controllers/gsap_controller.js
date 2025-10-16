9a015c260533b9dc98fbb9fdd0641748649a714f
kaleb fukuda <kalebfukudajp@gmail.com>
2025-10-16 17:11:47 +0900

    Added sections and some scroll effects

--- app/javascript/controllers/gsap_controller.js
+++ app/javascript/controllers/gsap_controller.js
@@ -1,20 +1,48 @@
 import { Controller } from "@hotwired/stimulus"
 import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
 
 // Connects to data-controller="gsap"
 export default class extends Controller {
   connect() {
-      document.addEventListener("turbo:load", () => {
-      const title = document.querySelector("h1")
-      if (title) {
-        gsap.from(title, {
-          duration: 1,
-          y: -50,
-          opacity: 0,
-          ease: "power3.out"
-        })
-      }
-    })
+    //   document.addEventListener("turbo:load", () => {
+    //   const title = document.querySelector("h1")
+    //   if (title) {
+    //     gsap.from(title, {
+    //       duration: 1,
+    //       y: -50,
+    //       opacity: 0,
+    //       ease: "power3.out"
+    //     })
+    //   }
+    // })
+    const sections = this.element.querySelectorAll("section, .banner")
 
+    sections.forEach((section, index) => {
+      const nextSection = sections[index + 1]
+      if (!nextSection) return
+
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: section,
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+          markers: false, // set true to debug
+        }
+      })
+
+      tl.fromTo(section,
+        { opacity: 1, y: 0 },
+        { opacity: 0, y: -50, ease: "power1.out" }
+      )
+      tl.fromTo(nextSection,
+        { opacity: 0, y: 50 },
+        { opacity: 1, y: 0, ease: "power1.out" },
+        0 // start at same time as previous animation
+      )
+    })
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "animejs", to: "https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.es.js"
 pin "gsap", to: "https://esm.sh/gsap@3.13.0"
+pin "gsap/ScrollTrigger", to: "https://ga.jspm.io/npm:gsap@3.12.2/ScrollTrigger.js"
