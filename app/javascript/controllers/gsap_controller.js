73fa24c988a6a05196839982de2458be3daa33b7
kaleb fukuda <kalebfukudajp@gmail.com>
2025-10-20 12:55:05 +0900

    Finished first section

--- app/javascript/controllers/gsap_controller.js
+++ app/javascript/controllers/gsap_controller.js
@@ -7,44 +7,7 @@ gsap.registerPlugin(ScrollTrigger)
 // Connects to data-controller="gsap"
 export default class extends Controller {
   connect() {
-    //   document.addEventListener("turbo:load", () => {
-    //   const title = document.querySelector("h1")
-    //   if (title) {
-    //     gsap.from(title, {
-    //       duration: 1,
-    //       y: -50,
-    //       opacity: 0,
-    //       ease: "power3.out"
-    //     })
-    //   }
-    // })
     this.loadSVG();
-    // const sections = this.element.querySelectorAll("section, .banner")
-
-    // sections.forEach((section, index) => {
-    //   const nextSection = sections[index + 1]
-    //   if (!nextSection) return
-
-    //   const tl = gsap.timeline({
-    //     scrollTrigger: {
-    //       trigger: section,
-    //       start: "top top",
-    //       end: "bottom top",
-    //       scrub: true,
-    //       markers: false, // set true to debug
-    //     }
-    //   })
-
-    //   tl.fromTo(section,
-    //     { opacity: 1, y: 0 },
-    //     { opacity: 0, y: -50, ease: "power1.out" }
-    //   )
-    //   tl.fromTo(nextSection,
-    //     { opacity: 0, y: 50 },
-    //     { opacity: 1, y: 0, ease: "power1.out" },
-    //     0 // start at same time as previous animation
-    //   )
-    // })
   }
 
   loadSVG() {
@@ -60,13 +23,21 @@ export default class extends Controller {
   setAnimationScroll() {
     gsap.registerPlugin(ScrollTrigger);
     gsap.set("#svg_timetouch svg #Loading", { scaleX: 0, transformOrigin: "0 50%" });
+    gsap.set("#svg_timetouch svg #popup", {opacity: 0, transformOrigin: "0 50%"});
+
+    //Circle check
+    const checkBorder = document.getElementById("border");
+    const length = checkBorder.getTotalLength();
+
+    checkBorder.style.strokeDasharray = length;
+    checkBorder.style.strokeDashoffset = length;
 
     // Animate text
     gsap.from("#span_1", {
-      opacity: 1,         // começa invisível
-      x: -300,              // desloca 20px para baixo
+      opacity: 1,
+      x: -300,
       duration: 1.3,
-      ease: "power3.out"  // suaviza a animação
+      ease: "power3.out"
     });
 
     // Animate SVG timetouch
@@ -90,50 +61,49 @@ export default class extends Controller {
     });
 
 
-    runAnimation.add([
-      gsap.fromTo("#svg_timetouch svg",
+    runAnimation
+      .fromTo("#svg_timetouch svg",
         { scale: 1 },
         { opacity: 1, y: -350, scale: 1.2, duration: 5 }
-      ),
-       gsap.to("#span_1", {
-        opacity: 0,
-        y: -20,
-        duration: 3,
-        ease: "power2.out"
-      }),
-      gsap.to("#svg_timetouch svg #Loading", {
-        scaleX: 1,
-        duration: 5,
-        delay: 3.5,
-        ease: "power5.out"
+      )
+       .to("#span_1", {
+          opacity: 0,
+          y: -20,
+          duration: 3,
+          ease: "power2.out"
+        },
+        "<1")
+      .fromTo("#svg_timetouch svg #clicker",
+        { opacity: 0 },
+        { opacity:1, ease: "power5.in", duration: 3},
+        ">")
+      .to("#svg_timetouch svg #Loading", {
+          scaleX: 1,
+          duration: 5,
+          ease: "power5.out",
+        }, ">")
+      .fromTo(["#svg_timetouch svg #popup", "#svg_timetouch svg #background"], {
+          opacity: 0
+        } ,
+        {
+          opacity: 1,
+          ease: "power2.out",
+          duration: 3,
+        }, ">")
+      .to(checkBorder, {
+          strokeDashoffset: 0,
+          stroke: "#B4DFDD",
+          duration: 5,
+          ease: "power2.inOut"
+        }, ">")
+      .to("#svg_timetouch svg #popup", {
+        scale: 5,
+        ease: "power5.inOut",
+        duration: 6,
+        transformOrigin: "center center"
       })
-    ]);
-
-
-
-
-
-    // gsap.to("#bg_lamp svg #path29", {
-    //     x: 200, // Move 200 pixels to the right
-    //     rotation: 360, // Rotate 360 degrees
-    //     delay: 0.5,
-    //     duration: 1, // Animation duration (this will be controlled by scroll)
-    //   })
-
-    // runAnimation.to("#bg_lamp", {
-    //     scale: 1.3,
-    //     opacity: 0,
-    //     transformOrigin: "center center",
-    //     ease: "power2.inOut",
-    //   })
-
-
-    // runAnimation.fromTo(
-    //     "#banner_section",
-    //     { scale: 0.8, opacity: 0 },
-    //     { scale: 1, opacity: 1, ease: "power2.inOut" },
-    //     0 // simultâneo
-    //   )
-
+      .to(["#svg_timetouch svg #check", "#svg_timetouch svg #text"],
+        { opacity: 0}, ">"
+      );
   }
 }
