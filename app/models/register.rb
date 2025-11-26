58d76bb57aa2edee03204aa96f70f1e61c4cbf6f
kaleb fukuda <kalebfukudajp@gmail.com>
2025-11-26 16:37:47 +0900

    Solved bug when registering extra hour and cost

--- app/models/register.rb
+++ app/models/register.rb
@@ -1,8 +1,17 @@
 class Register < ApplicationRecord
+  before_validation :set_default_values
+
   belongs_to :gemba
   belongs_to :period
   belongs_to :profile
   validates :date, presence: true
   validates :extra_hour, numericality: { greater_than_or_equal_to: 0 }
   validates :extra_cost, numericality: { greater_than_or_equal_to: 0 }
+
+  private
+
+   def set_default_values
+    self.extra_hour = 0.0 if extra_hour.blank?
+    self.extra_cost = 0 if extra_cost.blank?
+  end
 end
