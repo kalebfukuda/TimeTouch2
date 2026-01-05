f875ea7d55670390a17621352611175c2e06618f
kaleb fukuda <kalebfukudajp@gmail.com>
2026-01-05 19:56:53 +0900

    Added month and year as input for export

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,17 @@
 module ApplicationHelper
   def month_year_list(from = 2000)
-    start = Date.new(from,1,1)
+    start = Date.new(from, 1, 1)
     (start..Date.today).select { |d| d.day == 1 }
                       .map { |d| [d.strftime("%m/%Y"), d.strftime("%Y-%m")] }
                       .reverse
+  end
+
+  def year_list(from = 2024)
+    current_year = Date.today.year
+    (from..current_year).to_a.reverse
+  end
+
+  def month_list
+    Date::MONTHNAMES.compact.each_with_index.map { |name, index| [name, index + 1] }
   end
 end
